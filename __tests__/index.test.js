58130da4afbd87e4fa5e940d8c3dd813e316d0d0
Elison Santos <elison.s.ferreira@gmail.com>
2024-09-25 07:12:33 -0300

    atividade concluída

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,29 +4,50 @@ const {
   multiplicar,
   dividir,
   ehPar,
-} = require("../src/index");
+} = require("../src/index.js");
 
 // Testes para a função somar
-test("Soma dois números corretamente", () => {});
+test("Soma dois números corretamente", () => {
+  expect(somar( 2 , 0)).toBe(2);
+});
 
-test("Soma dois números negativos corretamente", () => {});
+test("Soma dois números negativos corretamente", () => {
+  expect(somar( -2 , -2)).toBe(-4);
+});
 
 // Testes para a função subtrair
-test("Subtrai dois números corretamente", () => {});
+test("Subtrai dois números corretamente", () => {
+  expect(subtrair( 2 , 0)).toBe(2);
+});
 
-test("Subtrai com resultado negativo", () => {});
+test("Subtrai com resultado negativo", () => {
+  expect(subtrair( -2 , -2)).toBe(0);
+});
 
 // Testes para a função multiplicar
-test("Multiplica dois números corretamente", () => {});
+test("Multiplica dois números corretamente", () => {
+  expect(multiplicar( 5 , 2)).toBe(10);
+});
 
-test("Multiplica número por zero", () => {});
+test("Multiplica número por zero", () => {
+  expect(multiplicar( 5 , 0)).toBe(0);
+});
 
 // Testes para a função dividir
-test("Divide dois números corretamente", () => {});
+test("Divide dois números corretamente", () => {
+  expect(dividir( 10 , 2)).toBe(5);
+});
 
-test("Lança erro ao dividir por zero", () => {});
+test("Lança erro ao dividir por zero", () => {
+  expect(() => dividir(10, 0)).toThrow("Divisão por zero não é permitida");
+});
 
 // Testes para a função ehPar
-test("Verifica se o número é par", () => {});
+test("Verifica se o número é par", () => {
+  expect(ehPar(10)).toBe("é par");
+});
 
-test("Verifica se o número é ímpar", () => {});
+test("Verifica se o número é ímpar", () => {
+  expect(ehPar(5)).toBe("é impar");
+
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 // Função que soma dois números
-function somar() {}
+function somar(num1, num2) {
+  resultado = (num1) + (num2);
+  return resultado;
+  
+}
 
 // Função que subtrai dois números
-function subtrair() {}
+function subtrair(num1, num2) {
+  return num1 - (num2);
+}
 
 // Função que multiplica dois números
-function multiplicar() {}
+function multiplicar(num1, num2) {
+  return num1 * num2;
+}
 
 // Função que divide dois números, com verificação de divisão por zero
-function dividir() {}
+function dividir(num1, num2) {
+
+  if(num1 === 0 || num2 ===0){
+    throw new Error("Divisão por zero não é permitida");
+    
+  }else{
+  return num1 / num2;
+  }
+}
 
 // Função que verifica se um número é par
-function ehPar() {}
+function ehPar(num) {
+  if(num % 2 == 0){
+    return "é par";
+  }
+  else{
+    return "é impar";
+  }
+}
 
 // Exporta todas as funções para serem usadas nos testes
 module.exports = {
